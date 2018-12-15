f1703b6267efc42f7348230cc876e84f220048cc
nesc <limarevigor@gmail.com>
2018-12-15 14:53:01 +0200

    fixed for english alphabet

--- second.py
+++ second.py
@@ -1,3 +1,4 @@
+LEN_OF_EN_ALPH = 26
 class Text:
 
     def __init__(self, text):
@@ -9,7 +10,8 @@ class Text:
         for word in words_list:
             bias_str = ''
             for char in word:
-                bias_str += chr(ord(char) + bias)
+                new_char = chr(ord(char) + bias)
+                bias_str += new_char if 'z' >= new_char else chr(ord(new_char) - LEN_OF_EN_ALPH)
             result_string = f'{result_string} {bias_str}'
         return result_string
 
@@ -17,4 +19,4 @@ if __name__ == "__main__":
 
     text = Text("zzz")
 
-    print(text.find_bias_str())
+    print(text.find_bias_str(3))
